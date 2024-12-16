35f55f350eccf90bfebe250df4bd90ea85b9042c
David-M-GitHub <david.musa.3110@gmail.com>
2024-12-16 17:28:42 +0100

    okayyy

--- version.py
+++ version.py
@@ -1,2 +1,2 @@
-CURRENT_VERSION = "b0.1.2"  # Local version of ImInstant
+CURRENT_VERSION = "b0.1.3"  # Local version of ImInstant
 UPDATE_URL = "https://raw.githubusercontent.com/David-M-GitHub/ImInstant/refs/heads/master/Version.txt"
