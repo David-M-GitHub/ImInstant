b381f273537e8608f23fc98bf40d002cf7de6206
David-M-GitHub <david.musa.3110@gmail.com>
2024-12-17 11:41:10 +0100

    pre-push?

--- version.py
+++ version.py
@@ -1,2 +1,2 @@
-CURRENT_VERSION = "b0.2.22"  # Local version of ImInstant
+CURRENT_VERSION = "b0.2.23"  # Local version of ImInstant
 UPDATE_URL = "https://raw.githubusercontent.com/David-M-GitHub/ImInstant/refs/heads/master/VERSION.txt"
